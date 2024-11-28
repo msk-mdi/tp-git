a8a84d8ec5940148a808101b43b400f662ec1ab3
Meziane Adam <adammez93@outlook.fr>
2024-11-28 19:39:11 +0100

    1.1

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 // Paramètres de connexion à la base de données
 define('DB_SERVER', 'mysql-tp-git-dev.mysql.database.azure.com');    // Serveur de la base de données
-define('DB_USERNAME', '');                                           // Nom d'utilisateur de la base de données
-define('DB_PASSWORD', '');                                           // Mot de passe de la base de données
+define('DB_USERNAME', 'esgiuser');                                           // Nom d'utilisateur de la base de données
+define('DB_PASSWORD', 'P@ssw0rd123!');                                           // Mot de passe de la base de données
 define('DB_NAME', 'party_planner_db');                               // Nom de la base de données
 
 try {
